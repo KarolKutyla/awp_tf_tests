4f40db8012d9b2c9c8a9a862a3d88a6c4a9a69d1
Karol Kutyła <103074417+JonaszPawulon@users.noreply.github.com>
2026-08-17 19:52:39 +0200

    init

--- awp_tf_tests/datasets/v2/cifar_10.py
+++ awp_tf_tests/datasets/v2/cifar_10.py
@@ -1,9 +1,40 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+
+def load_normalized_cifar_dataset():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_train = x_train.astype(np.float32) / 255.0
+    x_test = x_test.astype(np.float32) / 255.0
+    mean = x_train.mean(axis=(0, 1, 2))
+    std = x_train.std(axis=(0, 1, 2))
+
+    def normalize(x, y):
+        x = (x - mean) / std
+        return tf.cast(x, tf.float32), tf.cast(y, tf.int32)
+
+    x_train, y_train = normalize(x_train, y_train)
+    x_test, y_test = normalize(x_test, y_test)
+
+    tf_train_ds = (
+        tf.data.Dataset.from_tensor_slices((x_train, y_train))
+        .shuffle(50000)
+        .map(_transform_train_as_in_a_research_paper, num_parallel_calls=tf.data.AUTOTUNE)
+        .batch(128, drop_remainder=False)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+    tf_test_ds = (
+        tf.data.Dataset.from_tensor_slices((x_test, y_test))
+        .batch(128, drop_remainder=False)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+    return tf_train_ds, tf_test_ds
 
 
 def load_cifar_dataset():
     (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
     tf_train_ds = (
         tf.data.Dataset.from_tensor_slices((x_train, y_train))
         .map(lambda x, y: (keras.applications.resnet_v2.preprocess_input(tf.cast(x, tf.float32)), tf.cast(y, tf.int32)))
